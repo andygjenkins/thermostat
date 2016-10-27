d02e17ee594275691d536e4b85bd9c247ee11a5d
Iain Kinnish <iainkinnish@googlemail.com>
2016-10-27 10:21:22 +0100

    Started jQuery exercises.

--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
+
+
+  $('#temperature-up').click(function() {
+    thermostat.upButton();
+    $('#temperature').text(thermostat.temperature);
+  });
+
+  $('#temperature-down').click(function() {
+    thermostat.downButton();
+    $('#temperature').text(thermostat.temperature);
+  });
+
+//  function updateTemperature() {
+//    $('#temperature').text(thermostat.temperature);
+//  };
 });
