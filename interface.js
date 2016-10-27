c95712f20108f9517720aaa6e27fac4412fdbe44
amidabrian51 <briankaty1@me.com>
2016-10-27 12:55:14 +0100

    Intergrated API's into the App

--- interface.js
+++ interface.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemp();
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=8a99df7bf7ef4a4a202732c392b2d240&units=metric', function(data) {
+    $('#outsideTemperature').text(data.main.temp);
+  });
+  $('#select-city').submit(function(event) {
+      event.preventDefault();
+      var city = $('#current-city').val();
+      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=8a99df7bf7ef4a4a202732c392b2d240&units=metric', function(data) {
+        $('#outsideTemperature').text(data.main.temp);
+      })
+  });
   $('#increaseTemp').on('click', function() {
     thermostat.increaseTemp();
     updateTemp();
