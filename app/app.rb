44a2ca7b6604fa0de91d869a6345acc032300910
souljuse <oppo.irene@gmail.com>
2016-10-28 17:30:04 +0200

    trying step 10

--- app/app.rb
+++ app/app.rb
@@ -4,9 +4,28 @@ require_relative 'data_mapper_setup'
 require 'sinatra/base'
 
 class Thermostat < Sinatra::Base
+
   get '/' do
     erb :index
   end
+
+  post "/settings" do
+    Settings.all.destroy
+    Settings.create(
+    # temperature: params[],
+                              city: params[:city],
+                              # power_saving_mode: params[]
+                              )
+    redirect "/"
+  end
+
+  get "/settings" do
+    # settings = Settings.last {
+    #   city: settings.city
+    # }
+  end
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
