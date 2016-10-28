b3a2bec18f6b36ffcfdcc2cd12953da12172fa5e
andy jenkins <andygjenkins@gmail.com>
2016-10-28 15:03:17 +0100

    Add fix for CSS and html

--- app/app.rb
+++ app/app.rb
@@ -5,9 +5,7 @@ require 'sinatra/base'
 
 class Thermostat < Sinatra::Base
   get '/' do
-    headers 'Access-Control-Allow-Origin' => '*'
-    'Hello Thermostat!'
-    File.read('index.html')
+    erb :index
   end
 
   # start the server if ruby file executed directly
